bc5b97c4fc790a7a313a1b40c8a06ad174908ed0
admin <admin@Persons-MBP.attlocal.net>
2023-01-25 08:33:49 -0600

    Setting up the React app

--- stacks/index.js
+++ stacks/index.js
@@ -2,6 +2,7 @@ import { StorageStack } from "./StorageStack";
 import { App } from "@serverless-stack/resources";
 import { ApiStack } from "./ApiStack";
 import { AuthStack } from "./AuthStack";
+import { FrontendStack } from "./FrontendStack";
 
 /**
  * @param {App} app
@@ -14,5 +15,5 @@ export default function main(app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack);
 }
